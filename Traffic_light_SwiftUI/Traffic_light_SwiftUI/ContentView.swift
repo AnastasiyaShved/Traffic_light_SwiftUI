29a23210d41b1327d7a052b18a4c3219fbe1cb59
AnastasiyaShved <pav159208@gmail.om>
2023-12-11 15:00:23 +0300

    created new project

---
+++ Traffic_light_SwiftUI/Traffic_light_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Traffic_light_SwiftUI
+//
+//  Created by Apple on 11.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Traffic_light_SwiftUI/Traffic_light_SwiftUI/Traffic_light_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_light_SwiftUIApp.swift
+//  Traffic_light_SwiftUI
+//
+//  Created by Apple on 11.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_light_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
